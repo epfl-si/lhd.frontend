dc6b0d6d9d47193087f5ffa250e8706e24e34c8d
JaavLex <alexandre.javet@hotmail.com>
2022-02-11 17:08:17 +0100

    [feat] fetching data via graphQL
    - Our app is now able to fetch data from our backend via graphQL
    
    Co-authored-by: Dominique Quatravaux <domq@users.noreply.github.com>

--- src/components/appTable.js
+++ src/components/appTable.js
@@ -10,7 +10,7 @@ import {
 } from '@mui/x-data-grid';
 import { useState, useMemo, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import uData from '../json/example.json';
+//import uData from '../json/example.json';
 import TableSmartbar from './Searchbar/TableSmartbar';
 
 export const columns = [
@@ -19,7 +19,7 @@ export const columns = [
 	// { field: 'sector', headerName: 'Sector', width: 130 },
 	// { field: 'floor', headerName: 'Floor', width: 130 },
 	// { field: 'room', headerName: 'Room', width: 130 },
-	{ field: 'location', headerName: 'Location', width: 130 },
+	{ field: 'location', headerName: 'Location', width: 230 },
 	{ field: 'designation', headerName: 'Designation', width: 130 },
 	{ field: 'cosec', headerName: 'Cosec', width: 130 },
 	{ field: 'responsible', headerName: 'Responsible', width: 130 },
@@ -38,6 +38,7 @@ export default function AppTable() {
 		{ type: 'error', text: 'You have no query filters to share !' },
 	];
 
+	const [tableData, setTableData] = useState(null);
 	const [paramsData, setParamsData] = useState([]);
 	const [optionsList, setOptionsList] = useState([]);
 	const [autoComplete, setAutoComplete] = useState();
@@ -65,7 +66,44 @@ export default function AppTable() {
 		setOpenNotification(true);
 	};
 
+	const fetchResults = async unusedForNow__searchParams => {
+		const results = await fetch('http://localhost:3001/', {
+			headers: {
+				accept: '*/*',
+				'accept-language': 'en-US,en;q=0.9,fr-CH;q=0.8,fr;q=0.7',
+				'content-type': 'application/json',
+				'sec-ch-ua':
+					'" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+				'sec-ch-ua-mobile': '?0',
+				'sec-ch-ua-platform': '"macOS"',
+				'sec-fetch-dest': 'empty',
+				'sec-fetch-mode': 'cors',
+				'sec-fetch-site': 'cross-site',
+			},
+			referrerPolicy: 'no-referrer-when-downgrade',
+			body: '{"query":"query ExampleQuery {\\n  rooms (where: { building: { equals: \\"BC\\"} }) {\\n    name\\n    occupancies { cosecs { name} }\\n  }\\n}","variables":{},"operationName":"ExampleQuery"}',
+			method: 'POST',
+			mode: 'cors',
+			credentials: 'omit',
+		});
+		const graphQLResponse = await results.json();
+		return graphQLResponse.data.rooms.map(e => ({
+			id: e.name,
+			room: e.name,
+			unit: 'XXX COO',
+			cosec: e.occupancies[0].cosecs[0].name,
+			floor: 'XXX -2',
+			sector: 'XXX H',
+			faculty: 'XXX SV2',
+			building: 'XXX 8',
+			institute: 'XXX Salesforce',
+			designation: 'XXX Storage',
+			responsible: 'XXX Ferdinande Fendt',
+		}));
+	};
+
 	const onLoad = () => {
+		// TODO: Will need to put a params checker (returns an error snackbar if url param does not exist).
 		let urlParams = new URLSearchParams(window.location.search);
 		if (urlParams.has('filters')) {
 			let filters = urlParams.get('filters').split(',');
@@ -73,6 +111,7 @@ export default function AppTable() {
 				filters.map(e => ({ value: e.split(':')[0], label: e.split(':')[1] }))
 			);
 		}
+		fetchResults();
 	};
 
 	useEffect(() => {
@@ -87,9 +126,17 @@ export default function AppTable() {
 		);
 	}, [history, optionsList]);
 
+	// ! fix eslint error.
+	async function reloadResults(filter) {
+		setTableData(await fetchResults(filter));
+	}
+
+	useEffect(reloadResults, []);
+
 	useEffect(() => {
+		if (!tableData) return;
 		setAutoComplete(
-			uData
+			tableData
 				.map(e =>
 					Object.entries(e).map(i => {
 						const [lab, val] = i;
@@ -103,7 +150,9 @@ export default function AppTable() {
 				)
 				.filter(e => e.label !== 'id')
 		);
-	}, []);
+	}, [tableData]);
+
+	const Throbber = () => <p>This space unintentionnally left unblank</p>;
 
 	return (
 		<Box display="flex" flexDirection="column" alignItems="center">
@@ -118,7 +167,11 @@ export default function AppTable() {
 				/>
 			</Box>
 			<Box width="100%" height="500px">
-				<EntriesTableCategory optionsList={optionsList} />
+				{tableData !== null ? (
+					<EntriesTableCategory optionsList={optionsList} tableData={tableData} />
+				) : (
+					<Throbber />
+				)}
 			</Box>
 			<Box width="100%" paddingY="16px">
 				<Button label="Copy link w/ parameters" onClickFn={onShare} />
@@ -150,8 +203,8 @@ function CustomToolbar() {
 	);
 }
 
-function EntriesTableCategory({ optionsList }) {
-	const formattedData = uData.map(entry => ({
+function EntriesTableCategory({ optionsList, tableData }) {
+	const formattedData = tableData.map(entry => ({
 		id: entry.id,
 		location: `${entry.building} ${entry.sector} ${entry.floor} ${entry.room}`,
 		designation: entry.designation,
