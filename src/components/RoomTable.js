e461c4d6d9b46e9b6945bebcb5a712f4aacde628
JaavLex <alexandre.javet@hotmail.com>
2022-04-01 15:58:57 +0200

    [feat] Front is now getting all it's datas from graphql
    Co-authored-by: Dominique Quatravaux <domq@users.noreply.github.com>

---
+++ src/components/RoomTable.js
@@ -0,0 +1,36 @@
+import { AppTable } from '../components/AppTable';
+
+export function RoomTable() {
+	return (
+		<AppTable
+			graphqlBody={`  rooms (where: { building: { equals: "BC"} }) {
+				name
+				occupancies { 
+          cosecs { name }
+          professor { name }
+          unit {
+            name_unit
+            institut {
+              name_institut
+              faculty {
+                name_faculty
+              }
+            }
+            subunpro {
+              person {
+                name
+              }
+            }
+          }
+        }
+        building
+        sector
+        floor
+        kind {
+          name
+        }
+			}`}
+			variables={{}}
+		/>
+	);
+}
--- src/components/Searchbar/SmartbarParams.js
+++ src/components/Searchbar/SmartbarParams.js
@@ -1,7 +1,7 @@
 import { Box, Chip, IconButton, ListItem } from '@material-ui/core';
 import { DeleteForever } from '@mui/icons-material';
 import { Paper } from '@mui/material';
-import { columns } from '../appTable';
+import { columns } from '../AppTable';
 
 export default function SmartbarParams(props) {
 	const { paramsData, setParamsData, setSearchOptions } = props;
