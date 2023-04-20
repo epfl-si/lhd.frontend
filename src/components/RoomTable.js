48ba9e57b878634ac0989998c83bd43c8907539c
Alexandre <alexandre.javet@hotmail.com>
2023-04-20 21:21:23 +0200

    [feature] Query props in app and homepage

--- src/components/RoomTable.js
+++ src/components/RoomTable.js
@@ -2,14 +2,17 @@ import { OIDCContext } from '@epfl-si/react-appauth';
 import { RoomAuth } from './RoomAuth';
 import { env } from '../utils/env';
 
-export function RoomTable() {
+export function RoomTable({ gQuery }) {
 	return (
 		<OIDCContext
-			authServerUrl={ env().REACT_APP_AUTH_SERVER_URL }
-			client={{ clientId: 'LHDv3', redirectUri: env().REACT_APP_HOMEPAGE_URL,
-                                  scope: env().REACT_APP_OPENID_SCOPE }}
+			authServerUrl={env().REACT_APP_AUTH_SERVER_URL}
+			client={{
+				clientId: 'LHDv3',
+				redirectUri: env().REACT_APP_HOMEPAGE_URL,
+				scope: env().REACT_APP_OPENID_SCOPE,
+			}}
 		>
-			<RoomAuth />
+			<RoomAuth gQuery={gQuery} />
 		</OIDCContext>
 	);
 }
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -3,7 +3,31 @@ import { RoomTable } from '../components/RoomTable';
 export default function HomePage() {
 	return (
 		<div>
-			<RoomTable />
+			<RoomTable
+				gQuery={`rooms (where: { building: { equals: "BC"} }) {
+					name
+					occupancies { 
+						cosecs { name }
+						professors { name }
+						unit {
+							name
+							institute {
+								name
+								school {
+									name
+								}
+							}
+							unitId
+						}
+					}
+					building
+					sector
+					floor
+					kind {
+						name
+					}
+				}`}
+			/>
 		</div>
 	);
 }
