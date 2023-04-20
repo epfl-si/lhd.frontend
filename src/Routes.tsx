071c72e70737b62a992403c715ddb167624504b5
Alexandre <alexandre.javet@hotmail.com>
2023-04-20 10:30:48 +0200

    [ref] Route.js => Routes.tsx

---
+++ src/Routes.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+type Props = {
+	children?: React.ReactNode;
+};
+
+const Routes: React.FC<Props> = ({ children }) => {
+	return (
+
+	);
+};
+
+export default Routes;
