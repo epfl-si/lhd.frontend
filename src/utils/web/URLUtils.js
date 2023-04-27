51d7c0e4f29371d3522d2914d3891c15de544bb0
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2023-04-27 10:49:42 +0200

    [fix] Bad assumptions on `window.location.pathname` being always `/`
    Still needs fixing:
    - update `window.history` as the search filter changes (instead of having a button to synthesisze and copy the URL)
    - actually make hyperlinks to parametered URLs work, which might in turn require stashing away the return URL while we authenticate

--- src/utils/web/URLUtils.js
+++ src/utils/web/URLUtils.js
@@ -1,7 +1,7 @@
 export const copyLinkParams = optionsList => {
 	optionsList?.length > 0 &&
 		navigator.clipboard.writeText(
-			`${window.location.origin}/?filters=${optionsList
+			`${window.location.origin}${window.location.pathname}?filters=${optionsList
 				.map(o => `${o.value}:${o.label}`)
 				.join(',')}`
 		);
@@ -11,8 +11,8 @@ export const setUrlParams = optionsList => {
 	window.history.replaceState(
 		null,
 		null,
-		optionsList.length > 0
-			? `/?filters=${optionsList.map(o => `${o.value}:${o.label}`).join(',')}`
-			: '/'
+		window.location.pathname + optionsList.length > 0
+			? `?filters=${optionsList.map(o => `${o.value}:${o.label}`).join(',')}`
+			: ''
 	);
 };
