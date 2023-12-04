d453b33b1e2c690fd664d9cebf1cf7c4f0eec1f2
Dominique Quatravaux <dominique@quatravaux.org>
2023-12-04 14:37:15 +0100

    [feature] Bye `create-react-app`, welcome esbuild
    - Write a `esbuild.mjs` script to build all the things under `dist/`
    - Embed a reverse proxy in there, as advised [in the esbuild documentation](https://esbuild.github.io/api/#serve-proxy). Its job is to serve `public/index.html` except on select URIs (the ones we know that esbuild can handle by itself)
    - Support development and production builds
    - Trim all the create-react-app-ey bells and whistles out of `public/index.html`
    - Load the JS explicitly as part of a `<script defer>` in same `public/index.html` (this used to be done implicitly with `create-react-app`)
    - Add `/dist/` to `.gitignore`

---
+++ esbuild.mjs
@@ -0,0 +1,80 @@
+import * as esbuild from 'esbuild';
+import * as process from 'node:process';
+import { basename } from 'node:path';
+import inlineImage from 'esbuild-plugin-inline-image';
+import http from 'node:http';
+import { readFile } from 'node:fs/promises';
+
+const cmd = process.argv[process.argv.length - 1];
+
+function usage() {
+  console.log(`Usage: esbuild.mjs build | dev`);
+  process.exit(1);
+}
+
+const commonBuildConfig = {
+  entryPoints: ['src/index.js'],
+  bundle: true,
+  outfile: 'dist/lhd3-frontend.js',
+  loader: {
+    ".js": "jsx",
+  },
+  plugins: [inlineImage()],
+};
+
+if (cmd === "build") {
+  catchy(() => esbuild.build({
+    color: true,
+    logLevel: 'info',
+    minify: true,
+    ...commonBuildConfig
+  }));
+} else if (cmd === "dev") {
+  catchy(devServer);
+} else {
+  usage();
+}
+
+async function catchy (continuation) {
+  try {
+    await continuation();
+  } catch (e) {
+    console.error(e);
+    process.exit(2);
+  }
+}
+
+async function devServer () {
+  const ctx = await esbuild.context({
+    ...commonBuildConfig
+  });
+
+  // https://esbuild.github.io/api/#serve-proxy
+  let server = await ctx.serve();
+
+  function doProxy(req, res) {
+    const options = {
+      hostname: server.host,
+      port: server.port,
+      path: req.url,
+      method: req.method,
+      headers: req.headers,
+    }
+    const proxyReq = http.request(options, proxyRes => {
+      res.writeHead(proxyRes.statusCode, proxyRes.headers)
+      proxyRes.pipe(res, { end: true })
+    });
+    req.pipe(proxyReq, { end: true });
+  }
+
+  await http.createServer(async (req, res) => {
+    if (req.url.endsWith('/lhd3-frontend.js')) {
+      doProxy(req, res);
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(await readFile('public/index.html'));
+    }
+  }).listen(3000);
+
+  console.log("🚀 Browse the LHD frontend at http://localhost:3000/");
+}
