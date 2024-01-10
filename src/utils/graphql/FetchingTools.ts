1c79cc2f5b74f48c77eb3ab54ba4f0df8f48904f
Rosa Maggi <rosa.maggi@epfl.ch>
2024-01-10 12:00:37 +0100

    [Feature] get unit details

--- src/utils/graphql/FetchingTools.ts
+++ src/utils/graphql/FetchingTools.ts
@@ -149,6 +149,69 @@ export const fetchRoomDetails = async (
 	};
 };
 
+export const fetchUnitDetails = async (
+	address: string | undefined,
+	authToken: string | undefined,
+	unit: string | null,
+	variables: Object
+): Promise<fetchUnitsType> => {
+	const operationName = 'SingleUnitFetch';
+	debugger;
+	const results =
+		typeof address === 'string'
+			? await fetch(address, {
+				headers: {
+					accept: '*/*',
+					'content-type': 'application/json',
+					'sec-fetch-dest': 'empty',
+					'sec-fetch-mode': 'cors',
+					'sec-fetch-site': 'cross-site',
+					authorization: `Bearer ${authToken}`,
+				},
+				referrerPolicy: 'no-referrer-when-downgrade',
+				body: JSON.stringify({
+					query: `query ${operationName} { 
+						units (where: {name: { equals: "${unit}"} }) {
+							unitId
+								name
+								institute {
+									name
+									school {
+										name
+									}
+								}
+								cosecs {
+									name
+									surname
+									sciper
+								}
+								professors {
+									name
+									surname
+									sciper
+								}
+						},
+					}`,
+					variables,
+				}),
+				method: 'POST',
+				mode: 'cors',
+				credentials: 'omit',
+			})
+			: null;
+
+	if (results?.status !== 200) {
+		return { status: results?.status, data: await results?.text() };
+	}
+
+	const graphQLResponse = await results.json();
+
+	return {
+		status: results.status,
+		data: graphQLResponse.data?.units,
+	};
+};
+
 
 export const fetchRoomTypes = async (
 	address: string | undefined,
