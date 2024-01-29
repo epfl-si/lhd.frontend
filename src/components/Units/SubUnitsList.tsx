b50486d200dceee435ec90936bd4ee133d57d9cd
Rosa Maggi <rosa.maggi@epfl.ch>
2024-01-29 15:38:27 +0100

    [Fix] SubUnit style

--- src/components/Units/SubUnitsList.tsx
+++ src/components/Units/SubUnitsList.tsx
@@ -82,21 +82,28 @@ export const SubUnits = ({
 				/>
 			</div>
 			<div className="form-card-div">
-				{currentlySelected.map(item => {
-						return (<FormCard
-							key={item.name}
-							icon={item.status === 'Deleted' ? '#rotate-ccw' : '#trash-2'}
-							onClickIcon={() => onDelete(item)}
-							className={item.status === 'Deleted' ? 'form-card form-text-through' : (item.status === 'New' ? 'form-card form-card-dashed' : '')}
-						>
-							<div>
-								<small className="text-muted">
-									{item.name}
-								</small>
-							</div>
-						</FormCard>)
-					}
-				)}
+				<ul>
+					<li><i className="fa-angle-right rotate"></i>
+						<span><i className="far fa-gem ic-w mx-1"></i>{currentlySelected.length>0 ? parentName : t('unit_details.noSubUnits')}</span>
+						<ul className="nested">
+							{currentlySelected.map(item => {
+									return (<li><FormCard
+										key={item.name}
+										icon={item.status === 'Deleted' ? '#rotate-ccw' : '#trash-2'}
+										onClickIcon={() => onDelete(item)}
+										className={item.status === 'Deleted' ? 'form-card form-text-through' : (item.status === 'New' ? 'form-card form-card-dashed' : '')}
+									>
+										<div>
+											<small className="text-muted">
+												{item.name}
+											</small>
+										</div>
+									</FormCard></li>)
+								}
+							)}
+						</ul>
+					</li>
+				</ul>
 			</div>
 		</div>
 	);
