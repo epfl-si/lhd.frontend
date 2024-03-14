59738b2d12c3a2cd9f9c80f8fce3433958928a62
Rosa Maggi <rosa.maggi@epfl.ch>
2024-03-14 11:03:39 +0100

    [Refactor] move code in hazard form details page

--- src/components/global/FormDialog.tsx
+++ src/components/global/FormDialog.tsx
@@ -1,165 +1,161 @@
 import * as React from 'react';
 import {useEffect, useState} from 'react';
-import {hazardFormType} from "../../utils/ressources/types";
+import {hazardFormType, notificationType} from "../../utils/ressources/types";
 import {useTranslation} from "react-i18next";
 import {FormBuilder} from "@formio/react";
 import {Box, TextField, Typography} from "@material-ui/core";
 import {fetchHazardFormDetails} from "../../utils/graphql/FetchingTools";
 import {env} from "../../utils/env";
 import {useOpenIDConnectContext} from "@epfl-si/react-appauth";
-
-/*interface FormDialogProps {
-	openDialog: boolean;
-	row?: hazardFormType;
-	newHazard: boolean;
-	onClick?: (e: hazardFormType | undefined) => void;
-}*/
+import {Button} from "epfl-elements-react/src/stories/molecules/Button.tsx";
+import featherIcons from "epfl-elements/dist/icons/feather-sprite.svg";
+import {createNewHazardCategory, updateFormHazard} from "../../utils/graphql/PostingTools";
+import {notificationsVariants} from "../../utils/ressources/variants";
+import Notifications from "../Table/Notifications";
+import {useHistory} from "react-router-dom";
 
 export default function FormDialog() {
+	const history = useHistory();
 	const { t } = useTranslation();
 	const oidc = useOpenIDConnectContext();
-	const [form, setForm] = useState<hazardFormType>();
+	const [hazardFormDetails, setHazardFormDetails] = useState<hazardFormType>();
 	const [newForm, setNewForm] = useState<string>();
 	const [category, setCategory] = useState<string>('');
+	const [version, setVersion] = useState<string>('');
+	const [openNotification, setOpenNotification] = useState<boolean>(false);
+	const [notificationType, setNotificationType] = useState<notificationType>({
+		type: "info",
+		text: '',
+	});
 
+	const urlParams = new URLSearchParams(window.location.search);
 	useEffect(() => {
-		const loadFetch = async () => {
-			const urlParams = new URLSearchParams(window.location.search);
 			setCategory(urlParams.get('cat') ?? '');
 
-			if (urlParams.get('cat') == 'NewCategory') {
-
-			} else {
-				const results = await fetchHazardFormDetails(
-					env().REACT_APP_GRAPHQL_ENDPOINT_URL,
-					oidc.accessToken,
-					urlParams.get('cat') ?? ''
-				);
-				if (results.status === 200 && results.data && typeof results.data !== 'string' && results.data[0]) {
-					setForm(results.data[0]);
-					setNewForm(JSON.stringify(results.data[0].form))
-				} else {
-					console.error('Bad GraphQL results', results);
-				}
+			if (urlParams.get('cat') != 'NewCategory') {
+				loadFetch();
 			}
-		}
-		loadFetch();
 	}, [oidc.accessToken, window.location.search]);
 
-	const handleFormChange = (schema) => {
-		console.log('New Form Schema:', schema);
-		setNewForm(JSON.stringify(schema))
+	const loadFetch = async () => {
+		const results = await fetchHazardFormDetails(
+			env().REACT_APP_GRAPHQL_ENDPOINT_URL,
+			oidc.accessToken,
+			urlParams.get('cat') ?? ''
+		);
+		if (results.status === 200 && results.data && typeof results.data !== 'string' && results.data[0]) {
+			setHazardFormDetails(results.data[0]);
+			setNewForm(JSON.stringify(results.data[0].form));
+			setCategory(results.data[0].hazard_category.hazard_category_name);
+		} else {
+			console.error('Bad GraphQL results', results);
+		}
+	}
+
+	const handleSubmit = () => {
+		if (version != '' && category != '' && newForm && newForm != '') {
+			if (urlParams.get('cat') == 'NewCategory') {
+				createNewHazardCategory (
+					env().REACT_APP_GRAPHQL_ENDPOINT_URL,
+					oidc.accessToken,
+					{
+						id: JSON.stringify('{"salt":"newHazard","eph_id":"newHazard"}'),
+						form: JSON.stringify(newForm),
+						version: version,
+						hazard_category: {hazard_category_name: category}
+					}
+				).then(res => {
+					handleOpen(res);
+				});
+			} else {
+				updateFormHazard (
+					env().REACT_APP_GRAPHQL_ENDPOINT_URL,
+					oidc.accessToken,
+					{
+						id: JSON.stringify(hazardFormDetails?.id),
+						form: JSON.stringify(newForm),
+						version: version,
+						hazard_category: {hazard_category_name: category}
+					}
+				).then(res => {
+					handleOpen(res);
+				});
+			}
+		} else {
+			const notif: notificationType = {
+				text: t(`HazardFormControl.fillMandatoryFields`),
+				type: 'error'
+			};
+			setNotificationType(notif);
+			setOpenNotification(true);
+		}
+	}
+
+	const handleOpen = async (res: any) => {
+		if ( res.data?.updateForm?.errors ) {
+			const notif: notificationType = {
+				text: res.data?.updateForm?.errors[0].message,
+				type: 'error'
+			};
+			setNotificationType(notif);
+		} else if ( res.status === 200 ) {
+			history.push(`/formdetails?cat=${category}`);
+			setNotificationType(notificationsVariants['unit-update-success']);
+		} else {
+			setNotificationType(notificationsVariants['unit-update-error']);
+		}
+		setOpenNotification(true);
 	};
 
-	/*const handleClose = () => {
-		if ( onClick ) {
-			onClick(undefined);
-			setNewForm(undefined);
-		}
+	const handleClose = () => {
+		setOpenNotification(false);
 	};
-
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		const formData = new FormData(event.currentTarget);
-		const formJson = Object.fromEntries((formData as any).entries());
-		if ( onClick && newForm) {
-			onClick({id: JSON.stringify(category == 'NewCategory' || !row ? '{"salt":"newHazard","eph_id":"newHazard"}' : row.id),
-				form: newForm,
-				version: formJson.version,
-				hazard_category: {hazard_category_name: category == 'NewCategory' || !row ? formJson.category : (row.hazard_category.hazard_category_name ?? '')}});
-			setNewForm(undefined);
-		}
-	}*/
 
 	return (
 		<Box>
-			<Typography variant="h5" gutterBottom>{category == 'NewCategory' ? t(`HazardFormControl.Create`) : t(`HazardFormControl.Modify`)}</Typography>
-			<Typography variant="h6" style={{visibility: category == 'NewCategory' ? 'hidden' : 'visible'}}>
-				{form?.hazard_category.hazard_category_name}
+			<Typography variant="h5"
+									gutterBottom>{urlParams.get('cat') == 'NewCategory' ? t(`HazardFormControl.Create`) : t(`HazardFormControl.Modify`)}</Typography>
+			<Typography variant="h6" style={{visibility: urlParams.get('cat') == 'NewCategory' ? 'hidden' : 'visible'}}>
+				{hazardFormDetails?.hazard_category.hazard_category_name}
 			</Typography>
-			{category == 'NewCategory' && <TextField
-				autoFocus
-				required={category == 'NewCategory'}
-				margin="dense"
-				id="category"
-				name="category"
-				label={t(`HazardFormControl.newHazardCategory`)}
-				fullWidth
-				variant="standard"
-			/>}
+			{urlParams.get('cat') == 'NewCategory' && <TextField
+		  onChange={(event => {setCategory(event.target.value)})}
+		  autoFocus
+		  required={urlParams.get('cat') == 'NewCategory'}
+		  margin="dense"
+		  id="category"
+		  name="category"
+		  label={t(`HazardFormControl.newHazardCategory`)}
+		  fullWidth
+		  variant="standard"
+	  />}
 			<TextField
+				onChange={(event => {setVersion(event.target.value)})}
 				autoFocus
 				required
 				margin="dense"
 				id="version"
 				name="version"
-				label={category == 'NewCategory' ? t(`HazardFormControl.insertNewVersion`) : `${t(`HazardFormControl.newVersionCurrentIs`)} ${form?.version})`}
+				label={urlParams.get('cat') == 'NewCategory' ? t(`HazardFormControl.insertNewVersion`) : `${t(`HazardFormControl.newVersionCurrentIs`)} ${hazardFormDetails?.version})`}
 				fullWidth
 				variant="standard"
 			/>
 			<FormBuilder
-					form={(newForm || form?.form) ? JSON.parse(newForm ?? form!.form) : {}}
-					onChange={handleFormChange}
+				form={(newForm || hazardFormDetails?.form) ? JSON.parse(newForm ?? hazardFormDetails!.form) : {}}
+				onChange={(schema) => {setNewForm(JSON.stringify(schema))}}
+			/>
+			<div style={{marginTop: '50px'}}>
+				<Button
+					onClick={handleSubmit}
+					label="Save"
+					iconName={`${featherIcons}#save`}
+					primary/>
+			</div>
+			<Notifications
+				open={openNotification}
+				notification={notificationType}
+				close={handleClose}
 			/>
 		</Box>
-			/*<Dialog
-				fullScreen
-				open={openDialog}
-				onClose={handleClose}
-				PaperProps={{
-					component: 'form',
-					onSubmit: (event: React.FormEvent<HTMLFormElement>) => handleSubmit(event),
-				}}
-			>
-				<DialogTitle>{newHazard ? t(`HazardFormControl.Create`) : t(`HazardFormControl.Modify`)}</DialogTitle>
-				<DialogContent style={{width: '1000'}}>
-					<DialogContentText style={{visibility: newHazard ? 'hidden' : 'visible'}}>
-						{row?.hazard_category.hazard_category_name}
-					</DialogContentText>
-
-					{/!*<div style={{display: "flex", flexDirection: "row"}}>
-						<Textarea
-							onChange={(event => {
-								setNewForm(event.target.value);
-							})}
-							placeholder="Insert new form"
-							maxRows={20}
-							sx={{ minWidth: 500 }}
-							id="form"
-							name="form"
-							required
-							style={{marginRight: '20px'}}
-						/>
-						{newForm && <div style={{minWidth: 500}}><Form form={JSON.parse(newForm)}/></div>}
-					</div>*!/}
-					<FormBuilder
-						form={{
-							display: 'form',
-							components: [
-								{
-									"label": "Email",
-									"tableView": true,
-									"key": "email",
-									"type": "email",
-									"input": true
-								},
-								{
-									"label": "Password",
-									"tableView": false,
-									"key": "password",
-									"type": "password",
-									"input": true,
-									"protected": true
-								},
-							]
-						}}
-						onChange={handleFormChange}
-					/>
-				</DialogContent>
-				<DialogActions>
-				<Button onClick={handleClose}>{t(`HazardFormControl.cancel`)}</Button>
-					<Button type="submit">{t(`HazardFormControl.save`)}</Button>
-				</DialogActions>
-			</Dialog>*/
 	);
 }
--- src/pages/hazardFormControl.tsx
+++ src/pages/hazardFormControl.tsx
@@ -1,19 +1,13 @@
 import {useTranslation} from "react-i18next";
 import {useOpenIDConnectContext} from "@epfl-si/react-appauth";
 import React, {useEffect, useState} from "react";
-import {columnType, hazardFormType, lhdUnitsType, notificationType} from "../utils/ressources/types";
+import {columnType, hazardFormType} from "../utils/ressources/types";
 import {fetchHazardForms} from "../utils/graphql/FetchingTools";
 import {env} from "../utils/env";
 import {Box, Typography} from "@material-ui/core";
 import {EntriesTableCategory} from "../components/Table/EntriesTableCategory";
-import {GridRenderCellParams} from "@mui/x-data-grid";
 import featherIcons from "epfl-elements/dist/icons/feather-sprite.svg";
-import {FormDialog} from "../components/global/FormDialog";
-import {createNewHazardCategory, updateFormHazard} from "../utils/graphql/PostingTools";
-import {notificationsVariants} from "../utils/ressources/variants";
-import Notifications from "../components/Table/Notifications";
 import {Button} from "epfl-elements-react/src/stories/molecules/Button.tsx";
-import {FormBuilder} from "@formio/react";
 import {useHistory} from "react-router-dom";
 
 export default function HazardFormControl() {
@@ -21,15 +15,7 @@ export default function HazardFormControl() {
 	const oidc = useOpenIDConnectContext();
 	const history = useHistory();
 	const [tableData, setTableData] = useState<hazardFormType[]>([]);
-	const [row, setRow] = useState<hazardFormType>();
 	const [loading, setLoading] = useState(false);
-	const [newHazard, setNewHazard] = useState(false);
-	const [openDialog, setOpenDialog] = useState(false);
-	const [openNotification, setOpenNotification] = useState<boolean>(false);
-	const [notificationType, setNotificationType] = useState<notificationType>({
-		type: "info",
-		text: '',
-	});
 	const columns: columnType[] = [
 		{field: "version", headerName: t(`HazardFormControl.version`), width: 200},
 		{field: "hazard_category_name", headerName: t(`HazardFormControl.category`), width: 200, valueGetter: (params) => {
@@ -37,15 +23,7 @@ export default function HazardFormControl() {
 					return params.row.hazard_category.hazard_category_name;
 				}
 				return "";
-			}}/*,
-		{
-			field: "form", headerName: '', width: 30,
-			renderCell: (params: GridRenderCellParams<any, lhdUnitsType>) => (
-				<svg aria-hidden="true" className="icon feather" style={{margin: '3px'}} onClick={(event) => modifyForm(params.row)}>
-					<use xlinkHref={`${featherIcons}#edit-3`}></use>
-				</svg>
-			),
-		}*/
+			}}
 	];
 
 	useEffect(() => {
@@ -60,58 +38,10 @@ export default function HazardFormControl() {
 		);
 
 		if (resultsHazardCategory.status === 200 && resultsHazardCategory.data && typeof resultsHazardCategory.data !== 'string') {
+			console.log(resultsHazardCategory.data)
 			setTableData(resultsHazardCategory.data);
 		}
 		setLoading(false);
-	};
-
-	function modifyForm(row: hazardFormType) {
-		setOpenDialog(true);
-		setRow(row);
-		setNewHazard(false);
-	}
-
-	function saveForm(row: hazardFormType | undefined) {
-		setOpenDialog(false);
-		if (row) {
-			if (newHazard) {
-				createNewHazardCategory (
-					env().REACT_APP_GRAPHQL_ENDPOINT_URL,
-					oidc.accessToken,
-					row
-				).then(res => {
-					handleOpen(res);
-				});
-			} else {
-				updateFormHazard (
-					env().REACT_APP_GRAPHQL_ENDPOINT_URL,
-					oidc.accessToken,
-					row
-				).then(res => {
-					handleOpen(res);
-				});
-			}
-		}
-	}
-
-	const handleOpen = async (res: any) => {
-		if ( res.data?.updateForm?.errors ) {
-			const notif: notificationType = {
-				text: res.data?.updateForm?.errors[0].message,
-				type: 'error'
-			};
-			setNotificationType(notif);
-		} else if ( res.status === 200 ) {
-			setNotificationType(notificationsVariants['unit-update-success']);
-		} else {
-			setNotificationType(notificationsVariants['unit-update-error']);
-		}
-		setOpenNotification(true);
-		await loadFetch();
-	};
-
-	const handleClose = () => {
-		setOpenNotification(false);
 	};
 
 	return <Box>
@@ -128,20 +58,11 @@ export default function HazardFormControl() {
 			<div style={{marginTop: '50px'}}>
 				<Button
 					onClick={() => {
-						/*setOpenDialog(true);
-						setNewHazard(true);*/
 						history.push(`/formdetails?cat=NewCategory`);
 					}}
 					label={t(`HazardFormControl.addNewCategory`)}
 					iconName={`${featherIcons}#save`}
 					primary/>
 			</div>
-			{/*<FormDialog openDialog={openDialog} row={row}
-									onClick={saveForm} newHazard={newHazard}/>*/}
-			<Notifications
-				open={openNotification}
-				notification={notificationType}
-				close={handleClose}
-			/>
 		</Box>;
 }
