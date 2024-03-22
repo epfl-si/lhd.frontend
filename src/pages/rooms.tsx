e0c6e3272b151c15a7f75531094e9115edf5f757
Rosa Maggi <rosa.maggi@epfl.ch>
2024-03-22 15:50:20 +0100

    [Feature] make table and columns responsive

--- src/pages/rooms.tsx
+++ src/pages/rooms.tsx
@@ -2,7 +2,7 @@ import {useOpenIDConnectContext} from "@epfl-si/react-appauth";
 import React, {useEffect, useState} from "react";
 import {fetchRooms} from "../utils/graphql/FetchingTools";
 import {env} from "../utils/env";
-import {Box, Typography} from "@material-ui/core";
+import {Box, Typography, useMediaQuery} from "@material-ui/core";
 import {EntriesTableCategory} from "../components/Table/EntriesTableCategory";
 import {columnType, parameterType, roomDetailsType} from "../utils/ressources/types";
 import {useTranslation} from "react-i18next";
@@ -19,19 +19,25 @@ export default function RoomControl() {
 	const [search, setSearch] = React.useState('');
 	const [page, setPage] = useState<number>(0);
 	const [totalCount, setTotalCount] = useState<number>(0);
-	const columns: columnType[] = [
-			{field: "name", headerName: t('room.name'), width: 230},
-			{field: "building", headerName: t('room.building'), width: 230},
-			{field: "sector", headerName: t('room.sector'), width: 230},
-			{field: "floor", headerName: t('room.floor'), width: 230},
-			{field: "kind", headerName: t('room.kind.name'), width: 230, valueGetter: (params) => {
+
+	const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
+	const isMediumDevice = useMediaQuery("only screen and (min-width : 769px) and (max-width : 992px)");
+	const isLargeDevice = useMediaQuery("only screen and (min-width : 993px) and (max-width : 1200px)");
+	const isExtraLargeDevice = useMediaQuery("only screen and (min-width : 1201px)");
+
+	const columnsLarge: columnType[] = [
+			{field: "name", headerName: t('room.name'), width: 150},
+			{field: "building", headerName: t('room.building'), width: 100},
+			{field: "sector", headerName: t('room.sector'), width: 100},
+			{field: "floor", headerName: t('room.floor'), width: 100},
+			{field: "kind", headerName: t('room.kind.name'), width: 200, valueGetter: (params) => {
 				if (params.row.kind && params.row.kind.name) {
 					return params.row.kind.name;
 				}
 				return "";
 			}},
 			{
-				field: "hazardsListName", headerName: t('room_details.hazards'), width: 300,
+				field: "hazardsListName", headerName: t('room_details.hazards'), minWidth: 300,
 				renderCell: (params: GridRenderCellParams<any, roomDetailsType>) => (
 					<div className="displayFlexRow" style={{justifyContent: 'center'}}>
 						{
@@ -42,6 +48,58 @@ export default function RoomControl() {
 					</div>
 				),
 			}
+	];
+
+	const columnsMedium: columnType[] = [
+		{
+			field: "name", headerName: t('room.name'), minWidth: 300,
+			renderCell: (params: GridRenderCellParams<any, roomDetailsType>) => (
+				<span style={{lineHeight: '20px', fontSize: "smaller"}}>
+					<b>{params.row.name}</b>{` (${params.row.kind ? params.row.kind.name : ''})`}
+					<br/>
+					{`${(params.row.building ? params.row.building : '') + 
+					(params.row.sector ? (',' + params.row.sector) : '') + 
+					(params.row.floor ? (',' +params.row.floor) : '')}`}
+				</span>
+			),
+		},
+		{
+			field: "hazardsListName", headerName: t('room_details.hazards'), minWidth: 300,
+			renderCell: (params: GridRenderCellParams<any, roomDetailsType>) => (
+				<div className="displayFlexRow" style={{justifyContent: 'center'}}>
+					{
+						params.row.hazardsListName.map(c =>
+							<img style={{width: '30px', height: '30px', margin: '5px'}} key={`${c}_iconeKey`}
+									 src={getHazardImage(c)}/>)
+					}
+				</div>
+			),
+		}
+	];
+
+	const columnsSmall: columnType[] = [
+		{
+			field: "name", headerName: t('room.name'), minWidth: 300,
+			renderCell: (params: GridRenderCellParams<any, roomDetailsType>) => (
+				<div style={{lineHeight: '20px', fontSize: "smaller", display: "flex", flexDirection: 'column'}}>
+					<span>
+						<b>{params.row.name}</b>{` (${params.row.kind ? params.row.kind.name : ''})`}
+					</span>
+					<div style={{display: "flex", flexDirection: 'row'}}>
+						{`${(params.row.building ? params.row.building : '') +
+						(params.row.sector ? (',' + params.row.sector) : '') +
+						(params.row.floor ? (',' + params.row.floor) : '')}`}
+						<div className="displayFlexRow" style={{justifyContent: 'center', marginLeft: '10px'}}>
+							{
+								params.row.hazardsListName.map(c =>
+									<img style={{width: '20px', height: '20px', margin: '2px', marginLeft: '5px'}} key={`${c}_iconeKey`}
+											 src={getHazardImage(c)}/>)
+							}
+						</div>
+					</div>
+				</div>
+			),
+		}
 	];
 
 	useEffect(() => {
@@ -58,7 +116,7 @@ export default function RoomControl() {
 			PAGE_SIZE * newPage,
 			search
 		);
-		if ( results.status === 200 && results.data) {
+		if ( results.status === 200 && results.data ) {
 			const roomsList: roomDetailsType[] = results.data.rooms;
 			roomsList.forEach(r => {
 				const listCat = r.hazards.map(h => h.hazard_form_history.hazard_form.hazard_category.hazard_category_name);
@@ -94,7 +152,7 @@ export default function RoomControl() {
 			/>
 			<EntriesTableCategory
 				tableData={tableData}
-				columns={columns}
+				columns={(isExtraLargeDevice || isLargeDevice) ? columnsLarge : (isMediumDevice ? columnsMedium : columnsSmall)}
 				loading={loading}
 				pageToOpen={"room"}
 				loadServerRows={loadFetch}
