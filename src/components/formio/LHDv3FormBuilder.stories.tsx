87de7dc7400bfe11eb7f533f7afe1c5c1c2a4404
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2024-07-01 16:31:34 +0200

    [test] `LHDv3FormBuilder` (*and* the form it creates) with large lists

--- src/components/formio/LHDv3FormBuilder.stories.tsx
+++ src/components/formio/LHDv3FormBuilder.stories.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useState } from "react";
 import type { Meta, StoryObj } from '@storybook/react';
 import { LHDv3FormBuilder } from './LHDv3Forms';
+import { Form } from "@formio/react";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta : Meta<typeof LHDv3FormBuilder> = {
@@ -21,3 +23,33 @@ export const Simple: Story = {
     rooms: ['Room 1', 'Room 2']
   }
 };
+
+export const LargeList: Story = {
+  render() {
+    const [ lastForm, setLastForm ] = useState({});
+    const [ formShown, setFormShown ] = useState<boolean>(false);
+
+    const submission = { data: {
+      organism: "Organism 737",
+      rooms: "Room 311",
+      textArea: "Area 51"
+    } };
+
+    const multiplier = 1000;
+
+    return <div>
+             { formShown ? <Form form={lastForm} 
+                             submission={submission}/> : <></> }
+             <LHDv3FormBuilder
+               organisms={ multipliered((n) => `Organism ${n}`) }
+               rooms={ multipliered((n) => `Room ${n}`) }
+               onChange={(form) => setLastForm(form)}
+             />
+             <button onClick={() => setFormShown(true)}>Make a form!</button>
+    </div>;
+
+    function multipliered(pattern: (n: number) => string) : string[] {
+      return Array.from({ length: multiplier }, (v, i) => pattern(i + 1));
+    }
+  }
+}
