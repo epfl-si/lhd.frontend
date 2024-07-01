9d748042a29adabd8fcbc51c85747511485afb70
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2024-07-01 16:04:53 +0200

    [scaffold] Organism and room custom drop-downs

--- src/components/formio/LHDv3Forms.tsx
+++ src/components/formio/LHDv3Forms.tsx
@@ -11,9 +11,77 @@ export type LHDv3FormBuilderProps = {
 export const LHDv3FormBuilder : FC<LHDv3FormBuilderProps> = ({ onChange }) => {
   return <FormBuilder
            form={ {} }
+           options={
+	     {
+               builder: {
+	         custom: {
+		   title: 'LHD Fields',
+		   components: {
+                     organism: OrganismDropDownList.options,
+                     room: RoomDropDownList.options
+                   }
+	         }
+	       }
+             }
+           }
            onChange={ onChange }
          />
 
 }
 
 export type LHDv3FormType = any;
+
+export type OrganismDropDownListProps = {
+}
+
+export type FormBuilderOptions = any;
+
+export const OrganismDropDownList = Object.assign(
+  (({}) => {
+  }) as FC<OrganismDropDownListProps>,
+  {
+    options: {
+      title: 'Organism',
+      key: 'organism',
+      icon: 'terminal',
+      schema: {
+	label: 'Organism',
+	type: 'select',
+	key: 'organism',
+	input: true,
+	dataSrc: 'json',
+	template: "<span>{{ item.organism }}</span>",
+	data: {
+	  json: ['Or', 'Ga', 'Nism']
+	}
+      }
+    } as FormBuilderOptions
+  }
+);
+
+export type RoomDropDownListProps = {
+}
+
+export const RoomDropDownList = Object.assign(
+  (({}) => {
+  }) as FC<RoomDropDownListProps>,
+  {
+    options: {
+      title: 'Rooms',
+      key: 'rooms',
+      icon: 'terminal',
+      schema: {
+	label: 'Other impacted rooms',
+	type: 'select',
+	multiple: true,
+	key: 'otherImpactedRooms',
+	input: true,
+	dataSrc: 'json',
+	template: "<span>{{ item.name }}</span>",
+	data: {
+	  json: ['Room 1', 'Room 2']
+	}
+      },
+    } as FormBuilderOptions
+  }
+);
