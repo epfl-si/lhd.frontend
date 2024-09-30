c5e80f309064bb8ace6b824d0169686f696d8023
Dominique Quatravaux <dominique@quatravaux.org>
2024-09-30 12:23:10 +0200

    [fix] Import FontAwesome
    As per https://docs.fontawesome.com/apis/javascript/icon-library
    
    Font Awesome is a dependency of the form.io form builder.

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,12 @@ import {fetchConnectedUser} from "./utils/graphql/FetchingTools";
 import {env} from "./utils/env";
 import HazardFormChildDetails from "./pages/hazardformchilddetails";
 import "../css/styles.scss";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fas, far, fab);
 
 function App() {
 	const { t } = useTranslation();
