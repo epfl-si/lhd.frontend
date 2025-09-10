21ab80fa2f31064c0c913c25ea20c4c61a6ab562
Rosa Maggi <rosa.maggi@epfl.ch>
2025-09-10 12:22:55 +0200

    [feature] Add link to SNow on units

--- src/utils/lang/Dictionary.js
+++ src/utils/lang/Dictionary.js
@@ -47,6 +47,7 @@ i18next.use(initReactI18next).init({
 					addNewUnit: 'Add a new sub-unit',
 					noSubUnits: 'No sub-unit present',
 					editName: 'Edit the sub-unit name',
+					linkSnow: 'Link to unit safety tickets',
 					deleteUnitConfirmationMessageTitle: 'Are you sure to delete this unit: ',
 					deleteUnitConfirmationMessageDescription: 'All relationships with responsibles, COSECs, rooms and storages will be deleted. \nAll sub-units will be deleted too.'
 				},
@@ -304,6 +305,7 @@ i18next.use(initReactI18next).init({
 					addNewUnit: 'Ajouter une nouvelle sous-unité',
 					noSubUnits: 'Aucune sous-unité présente',
 					editName: 'Modifier le nom de la sous-unité',
+					linkSnow: 'Lien vers les tickets de l\'unité',
 					deleteUnitConfirmationMessageTitle: 'Êtes-vous sûrs de vouloir supprimer cette unité: ',
 					deleteUnitConfirmationMessageDescription: 'Toutes les rélations avec les responsables, les COSECs, les locaux et les stockages seront aussi supprimées, ainsi que toutes les sous-unités.'
 				},
