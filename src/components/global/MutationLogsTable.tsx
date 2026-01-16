272060d5b2dd124ffa2f38f5440d86297e5bf3b7
Rosa Maggi <rosa.maggi@epfl.ch>
2026-01-16 15:05:13 +0100

    [fix] History is now on a panel

--- src/components/global/MutationLogsTable.tsx
+++ src/components/global/MutationLogsTable.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import {splitCamelCase} from "../../utils/ressources/jsonUtils";
+import {ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary, Typography} from "@material-ui/core";
+import {useTranslation} from "react-i18next";
 
 interface MutationLogsTableProps {
 	history: any[];
@@ -8,40 +10,50 @@ interface MutationLogsTableProps {
 export const MutationLogsTable = ({
 	history
 }: MutationLogsTableProps) => {
-	return (history && history.length > 0 ? <table style={{ borderCollapse: "collapse", width: "100%" }}>
-		<thead>
-		<tr>
-			<th>Modified On</th>
-			<th>Modified By</th>
-			<th>Field</th>
-			<th>Before</th>
-			<th>After</th>
-		</tr>
-		</thead>
-		<tbody>
-		{history.map((group, groupIndex) =>
-			group.diffs.map((diff, diffIndex) => (
-				<tr key={`${groupIndex}-${diffIndex}`}
-						style={{
-							borderTop: diffIndex === 0 ? "1px solid rgb(183 180 180)" : "1px dashed #ddd"
-						}}>
-					{diffIndex === 0 && (
-						<>
-							<td rowSpan={group.diffs.length} style={{width: '10%'}} className="table-mutation-logs">
-								{group.modified_on}
-							</td>
-							<td rowSpan={group.diffs.length} style={{width: '10%'}} className="table-mutation-logs">
-								{group.modified_by}
-							</td>
-						</>
-					)}
+	const { t } = useTranslation();
 
-					<td style={{width: '15%'}} className="table-mutation-logs">{splitCamelCase(diff.field)}</td>
-					<td style={{width: '30%'}} className="table-mutation-logs">{String(diff.before ?? "–")}</td>
-					<td style={{width: '35%'}} className="table-mutation-logs">{String(diff.after ?? "–")}</td>
+	return (history && history.length > 0 ? <ExpansionPanel style={{width: '100%'}}>
+		<ExpansionPanelSummary expandIcon="▽" style={{backgroundColor: '#fafafa'}}>
+			<Typography style={{textDecoration: 'underline', textDecorationColor: 'red'}}>
+				{t('dispensation.history')}</Typography>
+		</ExpansionPanelSummary>
+		<ExpansionPanelDetails style={{display: "flex", flexDirection: "column"}}>
+			<table style={{ borderCollapse: "collapse", width: "100%" }}>
+				<thead>
+				<tr>
+					<th>Modified On</th>
+					<th>Modified By</th>
+					<th>Field</th>
+					<th>Before</th>
+					<th>After</th>
 				</tr>
-			))
-		)}
-		</tbody>
-	</table> : <></>)
+				</thead>
+				<tbody>
+				{history.map((group, groupIndex) =>
+					group.diffs.map((diff, diffIndex) => (
+						<tr key={`${groupIndex}-${diffIndex}`}
+								style={{
+									borderTop: diffIndex === 0 ? "1px solid rgb(183 180 180)" : "1px dashed #ddd"
+								}}>
+							{diffIndex === 0 && (
+								<>
+									<td rowSpan={group.diffs.length} style={{width: '10%'}} className="table-mutation-logs">
+										{group.modified_on}
+									</td>
+									<td rowSpan={group.diffs.length} style={{width: '10%'}} className="table-mutation-logs">
+										{group.modified_by}
+									</td>
+								</>
+							)}
+
+							<td style={{width: '15%'}} className="table-mutation-logs">{splitCamelCase(diff.field)}</td>
+							<td style={{width: '30%'}} className="table-mutation-logs">{String(diff.before ?? "–")}</td>
+							<td style={{width: '35%'}} className="table-mutation-logs">{String(diff.after ?? "–")}</td>
+						</tr>
+					))
+				)}
+				</tbody>
+			</table>
+		</ExpansionPanelDetails>
+	</ExpansionPanel> : <></>)
 }
