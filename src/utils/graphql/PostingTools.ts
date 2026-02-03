dd6d4c97fab4e3d335c73fe2bd095db36c8640a1
Rosa Maggi <rosa.maggi@epfl.ch>
2026-02-03 15:39:32 +0100

    [deadcode]

--- src/utils/graphql/PostingTools.ts
+++ src/utils/graphql/PostingTools.ts
@@ -10,63 +10,6 @@ import {
 } from '../ressources/types';
 import {doGraphQL} from "./Utils";
 
-export const createDispensation = async (
-	address: string | undefined,
-	authToken: string | undefined,
-	dispensation: dispensationRequestType,
-	variables: Object
-): Promise<any> => {
-	const operationName = 'newDispensation';
-	const results =
-		typeof address === 'string'
-			? await fetch(address, {
-					headers: {
-						accept: '*/*',
-						'content-type': 'application/json',
-						'sec-fetch-dest': 'empty',
-						'sec-fetch-mode': 'cors',
-						'sec-fetch-site': 'cross-site',
-						authorization: `Bearer ${authToken}`,
-					},
-					referrerPolicy: 'no-referrer-when-downgrade',
-					body: JSON.stringify({
-						query: `mutation ${operationName} {
-							createDispensation(subject: "${
-								dispensation.subject
-							}", author: "${'TEST'}", sciper_author: 312067, description: "${
-							escapeGraphQL(dispensation.requirements)
-						}", comment: "${escapeGraphQL(dispensation.comment)}", date_start: "${
-							dispensation.startDate
-						}", date_end: "${dispensation.endDate}", rooms: ${
-							dispensation.rooms
-						}, holders: ${dispensation.holders}) {
-								errors {
-									message
-								}
-								isSuccess
-								slug
-							}
-						}`,
-						variables,
-					}),
-					method: 'POST',
-					mode: 'cors',
-					credentials: 'omit',
-				})
-			: null;
-
-	if (results?.status !== 200) {
-		return { status: results?.status, data: await results?.text() };
-	}
-
-	const graphQLResponse = await results.json();
-
-	return {
-		status: results.status,
-		data: graphQLResponse.data,
-	};
-};
-
 const escapeGraphQL = (unquotedString: string): string => {
 	return unquotedString
 		.replaceAll(/("|\\)/g, '\\$1')
