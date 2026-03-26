f35a4505522856aa9426909f0097f0175ce9d2b9
Rosa Maggi <rosa.maggi@epfl.ch>
2026-03-26 10:19:32 +0100

    [fix] docker-entrypoint.sh already filled out all environment variables.

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,3 +1,8 @@
+/**
+ * window._12factor is filled by shell script at te beginning of the container
+ * see docker-entrypoint.sh for more information
+ * @returns {*}
+ */
 export function env() {
-  return process.env;
+  return window._12factor;
 }
