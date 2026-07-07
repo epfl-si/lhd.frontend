f52a90e31460a44aaa024b3a57c268f5e8fdcb70
Rosa Maggi <rosa.maggi@epfl.ch>
2026-07-07 17:23:48 +0200

    [feature] return empty for invalid date

--- src/utils/ressources/parser.ts
+++ src/utils/ressources/parser.ts
@@ -15,6 +15,8 @@ export function formatDate(date: Date, isForExport?: boolean) {
 }
 
 export function getFormattedDate(date: Date, split: String = '/') {
+	if (!date) return "";
+
 	const day = String(date.getDate()).padStart(2, '0');
 	const month = String(date.getMonth() + 1).padStart(2, '0');
 	const year = String(date.getFullYear());
